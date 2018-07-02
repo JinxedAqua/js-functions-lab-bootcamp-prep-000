ce218994c08d3c6f3612d8850caf41b33c3be29a
Jordan Andrew Thomas <thomas.jordan.andrew@gmail.com>
2018-07-02 13:08:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(name, holiday){
-  return `Happy ${holiday}, ${name}!`
-}
-function holidayCountdown(days, holiday){
-  return `It\'s ${days} days until ${holiday}!`
-}
